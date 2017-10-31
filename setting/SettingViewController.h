211520cbe393e6dacd14e01316cbb8753b14a156
GaoFei <redhat050676@gmail.com>
2017-10-31 15:43:45 +0800

    inital

---
+++ setting/SettingViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.h
+//  setting
+//
+//  Created by 高国富 on 2017/10/30.
+//  Copyright © 2017年 高国富. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+//static UILabel *settingLabel;
+//+ (void)changeLabelText:(NSString *)text;
+@end
---
+++ setting/SettingViewController.m
@@ -0,0 +1,43 @@
+//
+//  SettingViewController.m
+//  setting
+//
+//  Created by 高国富 on 2017/10/30.
+//  Copyright © 2017年 高国富. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@interface SettingViewController ()
+@end
+
+@implementation SettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+}
+//+ (void)changeLabelText:(NSString *)text{
+//    settingLabel = [[UILabel alloc]init];
+//    settingLabel.frame = self.view.bounds;
+//    settingLabel.text = text;
+//    [self.view addSubview:settingLabel];
+//
+//}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
